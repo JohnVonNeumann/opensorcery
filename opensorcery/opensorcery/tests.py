6239d338932807f33c157c527891155030c54853
JohnVonNeumann <louiswillcock@gmail.com>
2018-04-28 14:02:34 +1000

    UPDATE os/tests - fix test after swapping view to template return

--- opensorcery/opensorcery/tests.py
+++ opensorcery/opensorcery/tests.py
@@ -5,8 +5,10 @@ class OpensorceryTests(TestCase):
     def test_opensorcery_index_page_response(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        print(response)
+
 
     def test_opensorcery_index_page_response_body(self):
         response = self.client.get('/')
-        self.assertEqual(response.content, b"Welcome to Opensorcery")
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertContains(response, b"Welcome to Opensorcery")
+
