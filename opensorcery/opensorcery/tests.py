37a7afcdab8da85c2ba2467ac307b9d418913cc9
JohnVonNeumann <louiswillcock@gmail.com>
2018-04-28 12:08:11 +1000

    UPDATE os/tests - add assertion for reponse body

--- opensorcery/opensorcery/tests.py
+++ opensorcery/opensorcery/tests.py
@@ -5,3 +5,8 @@ class OpensorceryTests(TestCase):
     def test_opensorcery_index_page_response(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+        print(response)
+
+    def test_opensorcery_index_page_response_body(self):
+        response = self.client.get('/')
+        self.assertEqual(response.content, b"Welcome to Opensorcery")
