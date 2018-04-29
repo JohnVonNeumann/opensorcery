67069cf0a600cb549e7062da96e61401f9021b03
JohnVonNeumann <louiswillcock@gmail.com>
2018-04-30 07:58:27 +1000

    UPDATE auth0 views - rearrange the view funcs to work

--- opensorcery/auth0login/views.py
+++ opensorcery/auth0login/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+import json
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+@login_required
+def dashboard(request):
+    user = request.user
+    auth0user = user.social_auth.get(provider="auth0")
+    userdata = {
+        'user_id': auth0user.uid,
+        'name': user.first_name,
+        'picture': auth0user.extra_data['picture']
+    }
+    
+    return render(request, 'dashboard.html', {
+        'auth0User': auth0user,
+        'userdata': json.dumps(userdata, indent=4)
+    })
