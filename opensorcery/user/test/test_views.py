9a63e9320d62554cef4cff4f9a72d8434691960d
JohnVonNeumann <louiswillcock@gmail.com>
2018-04-30 23:12:18 +1000

    UPDATE test_views user - clean out unneeded tests, add comments

--- opensorcery/user/test/test_views.py
+++ opensorcery/user/test/test_views.py
@@ -3,26 +3,8 @@ from django.test import TestCase
 # Create your tests here.
 class UserTests(TestCase):
 
-    def test_login_page(self):
-        response = self.client.get('/user/login')
-        self.assertEqual(response.status_code, 200)
+    def test_user_page_after_direct_uri(self):
+        response = self.client.get('/user')
+        self.assertEqual(response.status_code, 301)
 
-    def test_login_page_template(self):
-        response = self.client.get('/user/login')
-        self.assertTemplateUsed(response, 'login.html')
-
-    def test_login_page_template_contents(self):
-        response = self.client.get('/user/login')
-        self.assertContains(response, b'Login page')
-
-    def test_post_login_welcome_page(self):
-        response = self.client.get('/user/welcome')
-        self.assertEqual(response.status_code, 200)
-
-    def test_post_login_welcome_template_rendered(self):
-        response = self.client.get('/user/welcome')
-        self.assertTemplateUsed(response, 'welcome.html')
-
-    def test_post_login_template_contents(self):
-        response = self.client.get('/user/welcome')
-        self.assertContains(response, b'Welcome to the landing page.')
+    # tests need to be included for the results of a successful auth, along with tests that correctly reference the location of the unauthed redirect
