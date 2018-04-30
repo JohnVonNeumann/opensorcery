3af9f37c7554eaa621b1aefa09f957377e60ba4f
JohnVonNeumann <louiswillcock@gmail.com>
2018-04-30 21:30:00 +1000

    UPDATE auth0 tests - tests: authurl acc_tok_url

--- opensorcery/auth0login/tests.py
+++ opensorcery/auth0login/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
+from auth0login.auth0backend import Auth0
 # Create your tests here.
+
+@override_settings(SOCIAL_AUTH_AUTH0_DOMAIN='test')
+class Auth0BackendTestCase(TestCase):
+    
+    def test_authorization_url(self):
+        client = Auth0()
+        url = client.authorization_url()
+        self.assertEqual(url, "https://test/authorize")
+
+    def test_access_token_url(self):
+        client = Auth0()
+        url = client.access_token_url()
+        self.assertEqual(url, "https://test/oauth/token")
