890b9fc01f18370ce72e171dcaca8943667469bb
JohnVonNeumann <louiswillcock@gmail.com>
2018-05-02 08:20:55 +1000

    UPDATE user test_views -add redirect assertions

--- opensorcery/user/test/test_views.py
+++ opensorcery/user/test/test_views.py
@@ -3,8 +3,19 @@ from django.test import TestCase
 # Create your tests here.
 class UserTests(TestCase):
 
-    def test_user_page_after_direct_uri(self):
-        response = self.client.get('/user')
-        self.assertEqual(response.status_code, 301)
+    def test_directly_accessing_user_page_without_auth(self):
+        response = self.client.get('/user/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/login/auth0?next=/user/", fetch_redirect_response=False)
 
     # tests need to be included for the results of a successful auth, along with tests that correctly reference the location of the unauthed redirect
+
+
+#    def test_logged_in_uses_correct_template(self):
+#        login = self.client.login(username='testuser1', password='12345')
+#        resp = self.client.get('/login/auth0/')
+#        print(resp.context)
+#        self.assertEqual(str(resp.context['auth0user']), 'testuser1')
+#        self.assertEqual(resp.status_code, 200)
+
+
