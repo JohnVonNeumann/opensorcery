4e869bb888d990728fdf02c76bab7bf2c4a2d845
JohnVonNeumann <louiswillcock@gmail.com>
2018-05-05 23:17:59 +1000

    UPDATE settings - add in django_pdb, midware + installed_apps

--- opensorcery/opensorcery/settings.py
+++ opensorcery/opensorcery/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
 # Application definition
 
 INSTALLED_APPS = [
+    'django_pdb',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_pdb.middleware.PdbMiddleware'
 ]
 
 ROOT_URLCONF = 'opensorcery.urls'
