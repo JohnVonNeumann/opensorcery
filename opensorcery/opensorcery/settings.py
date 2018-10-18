cca96eb852bdf626e8246c76b9cd2303fd6d16f1
JohnVonNeumann <louiswillcock@gmail.com>
2018-10-19 08:07:27 +1100

    UPDATE settings.py - switch database engine over to postgres"

--- opensorcery/opensorcery/settings.py
+++ opensorcery/opensorcery/settings.py
@@ -76,11 +76,13 @@ WSGI_APPLICATION = 'opensorcery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
