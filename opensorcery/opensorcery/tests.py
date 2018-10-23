98b6545506fe2fc5017b5086b59aebaf45fdb03f
JohnVonNeumann <louiswillcock@gmail.com>
2018-10-24 09:16:29 +1100

    UPDATE tests.py x2 - fix pycodestyle errs

--- opensorcery/opensorcery/tests.py
+++ opensorcery/opensorcery/tests.py
@@ -1,14 +1,13 @@
 from django.test import TestCase
 
+
 class OpensorceryTests(TestCase):
 
     def test_opensorcery_index_page_response(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-
     def test_opensorcery_index_page_response_body(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'index.html')
         self.assertContains(response, b"Welcome to Opensorcery")
-
