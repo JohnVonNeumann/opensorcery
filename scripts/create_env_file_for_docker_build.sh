27ca30bc6b0375e8d2de5a065cdc6550b78494ee
JohnVonNeumann <louiswillcock@gmail.com>
2019-04-07 23:24:08 +1000

    UPDATE scripts/create.sh - add docs to explain script

--- scripts/create_env_file_for_docker_build.sh
+++ scripts/create_env_file_for_docker_build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Automation script used as a workaround for testing `docker-compose build`
+# within the `travis` pipeline. From the local environment, users have a `.env`
+# file present for the `docker-compose` build to ingest and supply the docker
+# containers/django application with the required secrets. However, we can't
+# log this via git, as this file contains secrets. So we dynamically generate it
+# using the `before_script` travis directive, and source the environmental
+# variables from the travis admin keychain.
 touch .env
 echo "auth0_domain=${auth0_domain}" >> .env
 echo "api_identifier=${api_identifier}" >> .env
