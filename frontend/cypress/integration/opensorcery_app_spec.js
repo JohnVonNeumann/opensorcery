4c5674d412749aa0a9d785bde3c779a51a55d360
JohnVonNeumann <louiswillcock@gmail.com>
2019-04-07 14:33:53 +1000

    UPDATE frontend/ - update OOD deps, fix docker-compose up

--- frontend/cypress/integration/opensorcery_app_spec.js
+++
@@ -1,6 +0,0 @@
-describe('Opensorcery App', function () {
-  it('should() - assert that <title> is correct', function () {
-    cy.visit('localhost:8000')
-    cy.title().should('include', 'Opensorcery')
-  })
-})
