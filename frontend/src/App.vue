44c8a20ebd60c2c169d022ef52dff4c58fdf2302
JohnVonNeumann <louiswillcock@gmail.com>
2019-04-14 23:07:42 +1000

    UPDATE App.vue - remove secrets, get it working

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,68 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <button
+      class="btn btn-primary btn-margin"
+      v-if="!authenticated"
+      @click="login()">
+      Log In
+    </button>
+
+    <button
+      class="btn btn-primary btn-margin"
+      v-if="authenticated"
+      @click="privateMessage()">
+      Call Private
+    </button>
+
+    <button
+      class="btn btn-primary btn-margin"
+      v-if="authenticated"
+      @click="logout()">
+      Log Out
+    </button>
+    {{message}}
+    <br>
   </div>
 </template>
 
 <script>
+import AuthService from './auth/AuthService'
+import axios from 'axios'
+
+const API_URL = process.env.api_url
+const auth = new AuthService()
 export default {
-  name: 'App'
+  name: 'app',
+  data () {
+    this.handleAuthentication()
+    this.authenticated = false
+    auth.authNotifier.on('authChange', authState => {
+      this.authenticated = authState.authenticated
+    })
+    return {
+      authenticated: false,
+      message: ''
+    }
+  },
+  methods: {
+    // this method calls the AuthService login() method
+    login () {
+      auth.login()
+    },
+    handleAuthentication () {
+      auth.handleAuthentication()
+    },
+    logout () {
+      auth.logout()
+    },
+    privateMessage () {
+      const url = `${API_URL}/api/private/`
+      return axios.get(url, {headers: {Authorization: `Bearer ${AuthService.getAuthToken()}`}}).then((response) => {
+        console.log(response.data)
+        this.message = response.data || ''
+      })
+    }
+  }
 }
 </script>
 
