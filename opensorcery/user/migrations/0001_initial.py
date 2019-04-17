07987d007d7cc8974c0313bf8dd630a32cab5cda
JohnVonNeumann <louiswillcock@gmail.com>
2019-04-17 22:46:37 +1000

    LINT user_migs/001.py - fix pylint errors

--- opensorcery/user/migrations/0001_initial.py
+++ opensorcery/user/migrations/0001_initial.py
@@ -4,10 +4,18 @@ from django.conf import settings
 """
 TODO: Remove this migration once you have setup github SSO, it won't be needed
 """
+
+
 def create_data(apps, schema_editor):
     User = apps.get_model(settings.AUTH_USER_MODEL)
-    user = User(pk=1, username="auth0user", is_active=True , email="admin@techiediaries.com")
+    user = User(
+        pk=1,
+        username="auth0user",
+        is_active=True,
+        email="admin@techiediaries.com"
+    )
     user.save()
+
 
 class Migration(migrations.Migration):
     dependencies = [
