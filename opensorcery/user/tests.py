513e88031d05876088a77a21bf4fe8e863c30eba
JohnVonNeumann <louiswillcock@gmail.com>
2019-05-11 12:34:52 +1000

    UPDATE user - first TDD commit, create simple passing user model

--- opensorcery/user/tests.py
+++ opensorcery/user/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from .models import User
 
 # Create your tests here.
 class UserTests(TestCase):
@@ -7,3 +8,7 @@ class UserTests(TestCase):
     def test_userpage(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_user_creation(self):
+        user = User()
+        self.assertTrue(user)
