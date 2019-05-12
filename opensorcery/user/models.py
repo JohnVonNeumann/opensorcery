6e0e001c1f7f820d443d29fc6eed0099d31624f6
JohnVonNeumann <louiswillcock@gmail.com>
2019-05-12 20:40:21 +1000

    UPDATE user - add model, tests, migs

--- opensorcery/user/models.py
+++ opensorcery/user/models.py
@@ -3,4 +3,6 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
     user_name = models.CharField(max_length=50)
+    email = models.EmailField(default="")
--- opensorcery/user/tests.py
+++ opensorcery/user/tests.py
@@ -12,3 +12,19 @@ class UserTests(TestCase):
     def test_user_creation(self):
         user = User()
         self.assertTrue(user)
+
+    def test_user_username_attribute(self):
+        user = User(user_name="lol")
+        self.assertTrue(user.user_name)
+
+    def test_user_email_attribute(self):
+        user = User(email="lol")
+        self.assertTrue(user.email)
+
+    # test user doesnt have null values
+
+    # test username min and max length
+
+    # test user str representation
+
+    # test user has certain attributes
